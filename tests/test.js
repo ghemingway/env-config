5f569b6e144f9df4e5a07eb73104f18951cca028
ghemingway <graham.hemingway@gmail.com>
2018-03-08 16:03:09 -0600

    Initial commit.  Still lots to do.

---
+++ tests/test.js
@@ -0,0 +1,35 @@
+/* Copyright G. Hemingway, @2018 */
+"use strict";
+
+const envConfig = require("../index"),
+    testConfig = require("./config.json");
+
+
+describe("env-config", () => {
+
+    it("Success - default env - does nothing", async () => {
+        const config = await envConfig("./tests/config.json");
+        // TODO: Check that it is the expected config
+    });
+
+    it("Success - 'test' env", async () => {
+        let config = await envConfig("./tests/config.json", "test");
+        // TODO: Check that it is the expected config
+    });
+
+    it("Success - 'production' env", async () => {
+        let config = await envConfig("./tests/config.json", "production");
+        // TODO: Check that it is the expected config
+    });
+
+    it("Failure - config file does not exist", async () => {
+        try {
+            await envConfig("./not-a-config.json");
+        } catch(err) {
+            // TODO: Check that it is the expected error
+            return err;
+        }
+    });
+
+    it("Alternate flag", async () => {});
+});
